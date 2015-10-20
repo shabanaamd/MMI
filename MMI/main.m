8a28a7c67046aed31dc41be8e2002983e4f08655
shabanaamd <shabanaamd2011@gmail.com>
2015-10-20 15:48:45 +0530

    Initial Commit

---
+++ MMI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MMI
+//
+//  Created by Administrator on 20/10/15.
+//  Copyright © 2015 AtcomMobiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
